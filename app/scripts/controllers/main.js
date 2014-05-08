e3ccd56570f0bb20d24880805de62ac23e5c797e
Lior Spyer <lspyer@gmail.com>
2014-05-08 19:12:38 +0300

    using GET "/teams" in furet
    using the GET "/teams" route for furet client app.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,61 +1,16 @@
 'use strict';
 
 angular.module('furetApp')
-  .controller('MainCtrl', function ($scope, $timeout) {
+  .controller('MainCtrl', function ($scope, $timeout, $http) {
     $scope.gamePicked = false;
     $scope.team1 = '';
     $scope.team2 = '';
 
-    $scope.teams = [
-      // 3 Spain
-      'Real Madrid',
-      'Atletico Madrid',
-      'FC Barcelona',
-
-      // 3 England
-      'Chelsea',
-      'Liverpool',
-      'Arsenal',
-
-      'Manchester City',
-
-      // 3 Germany
-      'FC Bayern Munich',
-      'Bayer Leverkusen',
-      'Borussia Dortmund',
-
-      // 2 Italy
-      'Juventus',
-      'A.S. Rome',
-
-      'S.S.C. Napoli',
-
-      // 2 Portugal
-      'Benefica',
-      'Sporting Lisbon',
-
-      // 2 France
-      'PSG',
-      'Monaco',
-
-      // 1 Netherlands
-      'Ajax',
-
-      // // 1 Russia
-      // 'Zenit',
-
-      // // 1 Ukraine
-      // 'Shakhtar Donetsk',
-
-      // // 1 Belgium
-      // 'Standard Liege',
-
-      // // 1 Turkey
-      // 'Fenerbahce',
-
-      // // 1 Greece
-      // 'Olympiakos'
-    ];
+    $http({method: 'GET', url: "/teams"}).
+    success(function(data, status, headers, config) {
+      console.log(data)
+      $scope.teams = data
+    })
 
     var random = function(num) {
       return Math.floor(Math.random() * num);
